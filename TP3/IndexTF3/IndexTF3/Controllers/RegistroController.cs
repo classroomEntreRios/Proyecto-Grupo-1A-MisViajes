34f822e6bae6ce46712e0cf77e1aa9a0b9d0d38f
Juantus <juanp.caffa@gmail.com>
2021-04-16 17:10:23 -0300

    Nuevo BBDD

---
+++ TP3/IndexTF3/IndexTF3/Controllers/RegistroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IndexTF3.Controllers
+{
+    public class RegistroController : Controller
+    {
+        // GET: Registro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
